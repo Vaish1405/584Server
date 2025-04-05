3884d5b5c9c917e8cdc16dd1ea76e2b067b44775
Sen <vaishusen2002@gmail.com>
2025-04-04 17:34:45 -0700

    04/04 updates

---
+++ WEServer/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using cleanModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEServer.Dtos;
+using LoginRequest = WEServer.Dtos.LoginRequest;
+
+namespace WEServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController(UserManager<WorldCitiesUser> userManager, JwtHandler jwtHandler) : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult> LoginAsync(LoginRequest login)
+        {
+            WorldCitiesUser user = await userManager.FindByNameAsync(login.UserName);
+            if (user == null)
+            {
+                return Unauthorized("Unknown user");
+            }
+            bool success = await userManager.CheckPasswordAsync(user, login.Password);
+            if (!success)
+            {
+                return Unauthorized("Wrong Password");
+            }
+
+            JwtSecurityToken token = await jwtHandler.GetTokenAsync(user);
+
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new LoginResponse { 
+                Success = true, 
+                Message = "works", 
+                Token = tokenString
+            });
+        }
+    }
+}
--- WEServer/Program.cs
+++ WEServer/Program.cs
@@ -1,6 +1,10 @@
 using cleanModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using WEServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +17,32 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<WorldCitiesSourceContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        RequireAudience = true,
+        RequireExpirationTime = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateIssuerSigningKey = true,
+        
+        ValidIssuer = builder.Configuration["JwtSetting: Issuer"],
+        ValidAudience = builder.Configuration["JwtSetting: Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JwtSetting: SecurityKey"]))
+            ?? throw new InvalidOperationException()
+    };
+});
 
 builder.Services.AddIdentity<WorldCitiesUser, IdentityRole>()
     .AddEntityFrameworkStores<WorldCitiesSourceContext>();
+
+builder.Services.AddScoped<JwtHandler>();
 
 var app = builder.Build();
 
